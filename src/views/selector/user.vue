438c9e38c360b55227025a3b24fb72213b95c454
鬼王 <THRAEX@aliyun.com>
2020-12-13 17:06:42 +0800

    test(selector): radio

--- src/views/selector/user.vue
+++ src/views/selector/user.vue
@@ -12,6 +12,33 @@
         :visible.sync="case1.visible"
       />
     </thx-card-box>
+
+    <thx-card-box title="Case 2 - 用户单选(Callback)">
+      <template v-slot:toolbar>
+        <el-button type="danger" size="mini" @click="case2.user = null">清除</el-button>
+        <el-button type="primary" size="mini" @click="case2.visible = true">选择</el-button>
+      </template>
+      {{ case2.user || 'Unchecked' }}
+
+      <thx-user-selector
+        v-model="case2.user"
+        :visible.sync="case2.visible"
+        @ok="handleOkCase2"
+      />
+    </thx-card-box>
+
+    <thx-card-box title="Case 3 - 用户单选(有初始值)">
+      <template v-slot:toolbar>
+        <el-button type="danger" size="mini" @click="case3.user = null">清除</el-button>
+        <el-button type="primary" size="mini" @click="case3.visible = true">选择</el-button>
+      </template>
+      {{ case3.user || 'Unchecked' }}
+
+      <thx-user-selector
+        :value.sync="case3.user"
+        :visible.sync="case3.visible"
+      />
+    </thx-card-box>
   </div>
 </template>
 <script>
@@ -22,8 +49,26 @@ export default {
       case1: {
         visible: false,
         user: null
+      },
+      case2: {
+        visible: false,
+        user: null
+      },
+      case3: {
+        visible: false,
+        user: { id: '0028de89-2440-4b57-9b74-52ae6db387c8' }
       }
+    }
+  },
+  methods: {
+    handleOkCase2(data) {
+      this.case2.user = data
     }
   }
 }
 </script>
+<style scoped>
+.el-card {
+  margin-top: 10px;
+}
+</style>
