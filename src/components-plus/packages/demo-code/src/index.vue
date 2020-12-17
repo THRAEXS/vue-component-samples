c4aa8af83a15dd69c42c46714e662cfaa0a2a12f
鬼王 <THRAEX@aliyun.com>
2020-12-17 10:11:07 +0800

    feat(demo-code): transition tool

---
+++ src/components-plus/packages/demo-code/src/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-tabs type="border-card">
+    <el-tab-pane label="Demo">
+      <slot name="demo" />
+    </el-tab-pane>
+    <el-tab-pane label="Code">
+      <slot />
+    </el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  name: 'ThxDemoCode'
+}
+</script>
