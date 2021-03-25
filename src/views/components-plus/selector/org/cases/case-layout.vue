560e1c946d9a89d550dc2730b2352174908a2041
鬼王 <THRAEX@aliyun.com>
2021-03-25 11:35:08 +0800

    feat(org-selector): case layout

---
+++ src/views/components-plus/selector/org/cases/case-layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="case-layout">
+    <el-row :gutter="5">
+      <el-col :span="12">
+        <h4>弹框模式</h4>
+      </el-col>
+      <el-col :span="12">
+        <h4>级联模式</h4>
+      </el-col>
+    </el-row>
+    <el-row :gutter="5">
+      <el-col :span="12">
+        <slot name="left" />
+      </el-col>
+      <el-col :span="12">
+        <slot name="right" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<style scoped>
+.case-layout h4 {
+  margin: 5px 0;
+}
+.el-col {
+  /* border: 1px solid red; */
+}
+</style>
