72baa9337f65cef4d7535a3206021a5bea03425f
鬼王 <THRAEX@aliyun.com>
2021-05-12 10:47:54 +0800

    save(charts): flow

--- src/views/charts/flow.vue
+++ src/views/charts/flow.vue
@@ -25,74 +25,51 @@ export default {
       x: null,
       y: null,
       color: null,
-      names: null
+      keyframes: []
     }
   },
   created() {
-    // this.getData().then(data => (this.data = data)).then(() => {
-    //   // this.names = new Set(this.data.map(d => d.name))
-    // })
-
     this.x = d3.scaleLinear([0, 1], [this.margin.left, this.width - this.margin.right])
     this.y = d3.scaleBand()
       .domain(d3.range(this.n + 1))
       .rangeRound([this.margin.top, this.margin.top + this.barSize * (this.n + 1 + 0.1)])
       .padding(0.1)
 
-    this.color = (() => {
-      const scale = d3.scaleOrdinal(d3.schemeTableau10)
-      // if (this.data.some(d => d.category !== undefined)) {
-      //   const categoryByName = new Map(this.data.map(d => [d.name, d.category]))
-      //   scale.domain(categoryByName.values())
-      //   return d => scale(categoryByName.get(d.name))
-      // }
-      return d => scale(d.name)
-    })()
-
-    // this.rank = value => {
-    //   const data = Array.from(this.names, name => ({ name, value: value(name) }))
-    //   data.sort((a, b) => d3.descending(a.value, b.value))
-    //   for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(this.n, i)
-    //   return data
-    // }
+    this.color = (scale => d => scale(d.name))(d3.scaleOrdinal(d3.schemeTableau10))
   },
   mounted() {
-    // this.handleRaceV1()
-    // this.getData().then(data => (this.data = data.sort((a, b) => b.value - a.value))).then(() => {
-    this.getData().then(data => {
-      // for (let i = 0; i < 100; i++) {
-      //   data.push({
-      //     key: `key-name-${i}`,
-      //     name: `Name-${i}`,
-      //     value: Math.random() * 20000
-      //   })
-      // }
-      // this.names = new Set(this.data.map(d => d.name))
-      this.data.push(data.sort((a, b) => b.value - a.value))
-      for (let i = 0; i < 20; i++) {
-        this.data.push(data.map(it => {
-          const { value, ...other } = it
-          return { ...other, value: value * Math.random() * 1000 }
-        }).sort((a, b) => b.value - a.value))
-      }
-
-      // this.handleRaceV1()
-
+    this.getData().then(this.handleData).then(() => {
       // eslint-disable-next-line
       (async function(ag) { for await (const n of ag) { console.debug(n) } })(this.handleRace())
     })
   },
   methods: {
+    handleData(data) {
+      this.data = data
+
+      for (let i = 0; i < 20; i++) {
+        this.keyframes.push(data
+          .map(({ value, ...other }) => ({ ...other, value: Math.abs(value - Math.random() * 10000) }))
+          .sort((a, b) => b.value - a.value)
+        )
+      }
+
+      this.keyframes.push(data.sort((a, b) => b.value - a.value))
+    },
+    async getData() {
+      return (await request({ url: '/api/thraex/flows', method: 'get' })).data
+    },
     handleRace: async function * () {
       const svg = d3.select(this.$el.querySelector('svg'))
       svg.attr('viewBox', [0, 0, this.width, this.height])
 
       const updateBars = this.bars(svg)
-      // const updateLabels = this.labelsV1(svg)
+      const updateAxis = this.axis(svg)
+      // const updateLabels = this.labels(svg)
 
       yield svg.node()
 
-      for (const data of this.data) {
+      for (const data of this.keyframes) {
         const transition = svg.transition()
           .duration(this.duration)
           .ease(d3.easeLinear)
@@ -100,29 +77,11 @@ export default {
         this.x.domain([0, data[0].value])
 
         updateBars(data, transition)
+        updateAxis(data, transition)
         // updateLabels(data, transition)
 
         await transition.end()
       }
-    },
-    handleRaceV1() {
-      const svg = d3.select(this.$el.querySelector('svg'))
-      svg.attr('viewBox', [0, 0, this.width, this.height])
-
-      // svg.node()
-
-      // const transition = svg.transition()
-      //   .duration(this.duration)
-      //   .ease(d3.easeLinear)
-
-      // console.debug()
-      const d = this.data[0]
-      this.x.domain([0, Math.max(...d.map(({ value }) => value))])
-      // this.x.domain([0, this.data[0].value])
-
-      this.barsV1(svg)
-      this.labelsV1(svg)
-      // transition.end()
     },
     bars(svg) {
       let bar = svg.append('g')
@@ -136,57 +95,51 @@ export default {
             .attr('fill', this.color)
             .attr('height', this.y.bandwidth())
             .attr('x', this.x(0))
+            // .attr('y', d => this.y((this.prev.get(d) || d).rank))
             .attr('y', (_, i) => this.y(i))
+            // .attr('width', d => this.x((this.prev.get(d) || d).value) - this.x(0)),
             .attr('width', d => this.x(d.value) - this.x(0)),
           update => update,
           exit => exit.transition(transition).remove()
+            // .attr('y', d => this.y((this.next.get(d) || d).rank))
             .attr('y', (_, i) => this.y(i))
+            // .attr('width', d => this.x((this.next.get(d) || d).value) - this.x(0))
             .attr('width', d => this.x(d.value) - this.x(0))
-        ).call(bar => bar.transition(transition)
+        )
+        .call(bar => bar.transition(transition)
+          // .attr('y', d => this.y(d.rank))
           .attr('y', (_, i) => this.y(i))
-          .attr('width', d => this.x(d.value) - this.x(0))
-        )
-      )
+          // .attr('width', d => this.x(d.value) - this.x(0))))
+          .attr('width', d => this.x(d.value) - this.x(0))))
     },
-    barsV1(svg) {
-      let bar = svg.append('g')
-        .attr('fill-opacity', 0.6)
-        .selectAll('rect')
-        .data(this.data[0], d => d.name)
-        .join(
-          enter => enter.append('rect')
-            .attr('fill', this.color)
-            .attr('height', this.y.bandwidth())
-            .attr('x', this.x(0))
-            // .attr('y', d => this.y((this.prev.get(d) || d).rank))
-            .attr('y', (d, i) => this.y(i))
-            // .attr('width', d => this.x((this.prev.get(d) || d).value) - this.x(0)),
-            .attr('width', d => {
-              // console.debug((d.value) , (0))
-              // console.debug(this.x(d.value) , this.x(0))
-              return this.x(d.value) - this.x(0)
-            }),
-          // update => update,
-          // exit => exit.transition(transition).remove()
-          //   // .attr('y', d => this.y((this.next.get(d) || d).rank))
-          //   .attr('y', d => this.y(d.value))
-          //   // .attr('width', d => this.x((this.next.get(d) || d).value) - this.x(0))
-          //   .attr('width', d => this.x(d.value) - this.x(0))
-        )
-        // .call(bar => bar.transition(transition)
-        //   .attr('y', d => this.y(d.rank))
-        //   .attr('width', d => this.x(d.value) - this.x(0)))
+    axis(svg) {
+      const g = svg.append('g')
+        .attr('transform', `translate(0,${this.margin.top})`)
+
+      const axis = d3.axisTop(this.x)
+        .ticks(this.width / 160)
+        .tickSizeOuter(0)
+        .tickSizeInner(-this.barSize * (this.n + this.y.padding()))
+
+      return (_, transition) => {
+        g.transition(transition).call(axis)
+        g.select('.tick:first-of-type text').remove()
+        g.selectAll('.tick:not(:first-of-type) line').attr('stroke', 'white')
+        g.select('.domain').remove()
+      }
     },
-    labelsV1(svg) {
+    labels(svg) {
       let label = svg.append('g')
         .style('font', 'bold 12px var(--sans-serif)')
         .style('font-variant-numeric', 'tabular-nums')
         .attr('text-anchor', 'end')
         .selectAll('text')
-        .data(this.data[0], d => d.name)
+
+      return (data, transition) => (label = label
+        .data(data.slice(0, this.n), d => d.name)
         .join(
           enter => enter.append('text')
-            .attr('transform', (d, i) => `translate(${this.x(d.value)},${this.y(i)})`)
+            .attr('transform', d => `translate(${this.x((this.prev.get(d) || d).value)},${this.y((this.prev.get(d) || d).rank)})`)
             .attr('y', this.y.bandwidth() / 2)
             .attr('x', -6)
             .attr('dy', '-0.25em')
@@ -197,11 +150,20 @@ export default {
               .attr('x', -6)
               .attr('dy', '1.15em')),
           update => update,
-          // exit => exit.call(g => g.select('tspan').tween('text', d => d.value))
+          exit => exit.transition(transition).remove()
+            .attr('transform', d => `translate(${this.x((this.next.get(d) || d).value)},${this.y((this.next.get(d) || d).rank)})`)
+            .call(g => g.select('tspan').tween('text', d => this.textTween(d.value, (this.next.get(d) || d).value)))
         )
+        .call(bar => bar.transition(transition)
+          .attr('transform', d => `translate(${this.x(d.value)},${this.y(d.rank)})`)
+          .call(g => g.select('tspan').tween('text', d => this.textTween((this.prev.get(d) || d).value, d.value)))))
     },
-    async getData() {
-      return (await request({ url: '/api/thraex/flows', method: 'get' })).data
+    textTween(a, b) {
+      const i = d3.interpolateNumber(a, b)
+      const fn = d3.format(',d')
+      return function(t) {
+        this.textContent = fn(i(t))
+      }
     }
   }
 }
